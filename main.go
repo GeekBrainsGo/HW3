f89dd3a8f9dedf8a53544aa4e6c9f1acbbb61f2e
vbaykin <vbaykin@mail.ru>
2019-09-19 11:52:36 +0400

    sample

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/sirupsen/logrus"
+)
+
+func main() {
+	r := chi.NewRouter()
+
+	serv := Server{
+		Title: "TODO",
+		Tasks:[]string{
+			"Task 1",
+			"Task 2",
+			"Task 3",
+		},
+	}
+
+	// r.Handle("/", http.FileServer(http.Dir("./web/static/")))
+	r.Route("/", func(r chi.Router) {
+		r.Get("/", serv.HandleGetIndex)
+	})
+
+	http.ListenAndServe(":8080", r)
+}
+
+type Server struct {
+	Title string
+	Tasks []string
+}
+
+func (serv *Server) HandleGetIndex(w http.ResponseWriter, r *http.Request) {
+
+	file, _ := os.Open("./web/static/index.html")
+	data, _ := ioutil.ReadAll(file)
+
+	templ := template.Must(template.New("page").Parse(string(data)))
+	logrus.Info(templ.ExecuteTemplate(w, "page", serv))
+}
